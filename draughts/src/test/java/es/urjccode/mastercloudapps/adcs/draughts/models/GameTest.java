b187303a4ed454be0889811683dfcdac33c49e95
Setillo <setillofm@gmail.com>
2019-11-06 13:07:25 +0100

    Add test to GameTest for initialization

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameTest.java
@@ -12,6 +12,32 @@ public class GameTest {
         game = new Game();
     }
 
+    @Test
+    public void testGivenNewBoardThenGoodLocations() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < Board.DIMENSION; j++) {
+                Coordinate coordinate = new Coordinate(i,j);
+                Color color = game.getColor(coordinate);
+                if (coordinate.isBlack()){
+                    assertEquals(Color.BLACK, color);
+                } else {
+                    assertNull(color);
+                }
+            }
+        }
+        for (int i = 5; i < Board.DIMENSION; i++) {
+            for (int j = 0; j < Board.DIMENSION; j++) {
+                Coordinate coordinate = new Coordinate(i,j);
+                Color color = game.getColor(coordinate);
+                if (coordinate.isBlack()){
+                    assertEquals(Color.WHITE, color);
+                } else {
+                    assertNull(color);
+                }
+            }
+        }
+    }
+
     private Error advance(Coordinate[][] coordinates){
         Error error = null;
         for (int i = 0; i < coordinates.length; i++) {
