79617e287fc84752063fefb6a0ee448bb0c30595
Gabriel Enrique Macho <gabe3195@gmail.com>
2020-04-26 05:25:25 +0200

    ADDING TESTS TO HISTORY#1

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
@@ -1,9 +1,5 @@
 package es.urjccode.mastercloudapps.adcs.draughts.controllers;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
@@ -11,6 +7,9 @@ import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
 import es.urjccode.mastercloudapps.adcs.draughts.models.State;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Color;
 import es.urjccode.mastercloudapps.adcs.draughts.models.GameBuilder;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
 
 public class PlayControllerTest {
 
@@ -73,5 +72,125 @@ public class PlayControllerTest {
         assertEquals(Color.BLACK, playController.getColor());
         assertFalse(game.isBlocked());
     }
+
+    @Test
+    public void testGivenPlayControllerWhenDraughtsCanEatAndWillNotEatBlack() {
+        Game game = new GameBuilder().rows(
+            "     n n",
+            "  B n n ",
+            " n      ",
+            "      n ",
+            "   b    ",
+            "    b   ",
+            " b n   b",
+            "b b   b ").build();
+        playController = new PlayController(game, new State());
+        Coordinate origin = new Coordinate(5, 4);
+        Coordinate target = new Coordinate(4, 5);
+        playController.move(origin, target);
+        assertNull(playController.getPiece(new Coordinate(1, 2)));
+    }
+
+    @Test
+    public void testGivenPlayControllerWhenDraughtsCanEatAndWillNotEatWhite() {
+        Game game = new GameBuilder().rows(
+            "     n n",
+            "  B n n ",
+            " n      ",
+            "      n ",
+            "   b    ",
+            "    b   ",
+            " b N   b",
+            "b b   b ").build();
+        playController = new PlayController(game, new State());
+        Coordinate origin = new Coordinate(1, 2);
+        Coordinate target = new Coordinate(3, 0);
+        playController.move(origin, target);
+        origin = new Coordinate(3, 6);
+        target = new Coordinate(4, 7);
+        playController.move(origin, target);
+        assertNull(playController.getPiece(new Coordinate(6, 3)));
+    }
+
+    @Test
+    public void testGivenPlayControllerWhenPieceCanEatAndWillNotEatBlack() {
+        Game game = new GameBuilder().rows(
+            "        ",
+            "        ",
+            "   n    ",
+            "  b     ",
+            "     n  ",
+            "b       ",
+            "        ",
+            "  b     ").build();
+        playController = new PlayController(game, new State());
+        Coordinate origin = new Coordinate(5, 0);
+        Coordinate target = new Coordinate(4, 1);
+        playController.move(origin, target);
+        assertNull(playController.getPiece(new Coordinate(3, 2)));
+    }
+
+    @Test
+    public void testGivenPlayControllerWhenPieceCanEatAndWillNotEatWhite() {
+        Game game = new GameBuilder().rows(
+            "        ",
+            "        ",
+            "   n    ",
+            "        ",
+            " b    n ",
+            "b       ",
+            "        ",
+            "  b     ").build();
+        playController = new PlayController(game, new State());
+        Coordinate origin = new Coordinate(4, 1);
+        Coordinate target = new Coordinate(3, 2);
+        playController.move(origin, target);
+        origin = new Coordinate(4, 6);
+        target = new Coordinate(5, 5);
+        playController.move(origin, target);
+        assertNull(playController.getPiece(new Coordinate(2, 3)));
+    }
+
+    @Test
+    public void testGivenPlayControllerWhenPieceCanEatAndWillNotEatWhitesDouble() {
+        Game game = new GameBuilder().rows(
+            " n      ",
+            "        ",
+            "        ",
+            "    n n ",
+            "        ",
+            "b   b   ",
+            "        ",
+            "      b ").build();
+        playController = new PlayController(game, new State());
+        Coordinate origin = new Coordinate(5, 4);
+        Coordinate target = new Coordinate(4, 5);
+        playController.move(origin, target);
+        origin = new Coordinate(0, 1);
+        target = new Coordinate(1, 2);
+        playController.move(origin, target);
+        assertTrue(playController.getPiece(new Coordinate(3, 4)) == null
+            || playController.getPiece(new Coordinate(3, 6)) == null);
+    }
+
+    @Test
+    public void testGivenPlayControllerWhenPieceCanEatAndWillNotEatBlacksDouble() {
+        Game game = new GameBuilder().rows(
+            " n      ",
+            "     n  ",
+            "        ",
+            "        ",
+            "        ",
+            "  n     ",
+            " b b    ",
+            "  b b b ").build();
+        playController = new PlayController(game, new State());
+        Coordinate origin = new Coordinate(7, 6);
+        Coordinate target = new Coordinate(6, 7);
+        playController.move(origin, target);
+        assertTrue(playController.getPiece(new Coordinate(6, 1)) == null
+            || playController.getPiece(new Coordinate(6, 3)) == null);
+    }
+
 
 }
